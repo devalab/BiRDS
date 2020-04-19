f0272f861382616e1fba4db4c0453b3e314cc529
Vineeth Chelur <crvineeth97@gmail.com>
2020-04-19 22:40:24 +0530

    Add metrics and GRU model

---
+++ metrics.py
@@ -0,0 +1,92 @@
+import torch
+from torch.nn.functional import binary_cross_entropy_with_logits
+
+SMOOTH = 1e-6
+
+
+def IOU(output, target):
+    intersection = (output & target).float().sum()
+    union = (output | target).float().sum()
+    return (intersection) / (union)
+
+
+def CM(output, target):
+    # Get Confusion Matrix
+    cm = {}
+    cm["tp"] = (target * output).sum().float() + SMOOTH
+    cm["tn"] = (~target * ~output).sum().float() + SMOOTH
+    cm["fp"] = (~target * output).sum().float() + SMOOTH
+    cm["fn"] = (target * ~output).sum().float() + SMOOTH
+    return cm
+
+
+def MCC(cm):
+    return (cm["tp"] * cm["tn"] - cm["fp"] * cm["fn"]) / torch.sqrt(
+        (cm["tp"] + cm["fp"])
+        * (cm["tp"] + cm["fn"])
+        * (cm["tn"] + cm["fp"])
+        * (cm["tn"] + cm["fn"])
+    )
+
+
+def ACCURACY(cm):
+    return (cm["tp"] + cm["tn"]) / (cm["tp"] + cm["tn"] + cm["fp"] + cm["fn"])
+
+
+def RECALL(cm):
+    return cm["tp"] / (cm["tp"] + cm["fn"])
+
+
+def PRECISION(cm):
+    return cm["tp"] / (cm["tp"] + cm["fp"])
+
+
+def F1(cm):
+    precision = PRECISION(cm)
+    recall = RECALL(cm)
+    f1 = 2 * (precision * recall) / (precision + recall)
+    return f1
+
+
+def LOSS(output, target, pos_weight=None):
+    if pos_weight is None:
+        ones = (target == 1).float().sum()
+        zeros = len(target) - ones
+        pos_weight = [(zeros) / (ones)]
+    pos_weight = torch.Tensor(pos_weight)
+    if torch.cuda.is_available():
+        pos_weight = pos_weight.cuda()
+    return binary_cross_entropy_with_logits(output, target, pos_weight=pos_weight)
+
+
+def batch_loss(outputs, targets, lengths, pos_weight=None, reduction="mean"):
+    batch_size = len(lengths)
+    loss = 0.0
+    for i in range(batch_size):
+        loss += LOSS(outputs[i, : lengths[i]], targets[i, : lengths[i]], pos_weight)
+    if reduction == "mean":
+        loss /= batch_size
+    return loss
+
+
+def batch_metrics(outputs, targets, lengths, reduction="mean"):
+    batch_size = len(lengths)
+    for i in range(batch_size):
+        output = (torch.sigmoid(outputs[i, : lengths[i]]) > 0.5).bool()
+        target = targets[i, : lengths[i]].bool()
+        cm = CM(output, target)
+        metrics = {
+            "iou": IOU(output, target).item(),
+            "mcc": MCC(cm).item(),
+            "f1": F1(cm).item(),
+            "acc": ACCURACY(cm).item(),
+        }
+        if i == 0:
+            running_metrics = metrics
+            continue
+        for key in metrics:
+            running_metrics[key] += metrics[key]
+    if reduction == "mean":
+        for key in running_metrics:
+            running_metrics[key] /= batch_size
+    return running_metrics
--- models.py
+++ models.py
@@ -207,6 +207,7 @@ class BiLSTM(torch.nn.Module):
         self.num_layers = num_layers
         self.depth = len(hidden_sizes)
         self.brnn = torch.nn.ModuleList([])
+        self.dropout = torch.nn.Dropout(dropout, True)
         for i in range(self.depth):
             self.brnn.append(
                 torch.nn.LSTM(
@@ -237,6 +238,54 @@ class BiLSTM(torch.nn.Module):
             # [Max_length, Batch, 2 * hidden_sizes[i]] -> [Max_length, Batch, hidden_sizes[i]]
             output = output.view(lengths[0], batch_size, 2, self.hidden_sizes[i])
             output = output.mean(dim=2)
+            self.dropout(output)
+
+        # [Max_length, Batch, hidden_sizes[-1]] -> [Batch, Max_length]
+        output = output.transpose(0, 1).squeeze(dim=2)
+
+        return output
+
+
+class BiGRU(torch.nn.Module):
+    def __init__(self, feat_vec_len, hidden_sizes=[32, 1], num_layers=2, dropout=0.5):
+        super(BiGRU, self).__init__()
+        self.feat_vec_len = feat_vec_len
+        self.hidden_sizes = hidden_sizes
+        self.num_layers = num_layers
+        self.depth = len(hidden_sizes)
+        self.brnn = torch.nn.ModuleList([])
+        self.dropout = torch.nn.Dropout(dropout, True)
+        for i in range(self.depth):
+            self.brnn.append(
+                torch.nn.GRU(
+                    input_size=feat_vec_len,
+                    hidden_size=hidden_sizes[i],
+                    num_layers=num_layers,
+                    bias=True,
+                    dropout=dropout,
+                    bidirectional=True,
+                )
+            )
+            feat_vec_len = hidden_sizes[i]
+
+    def forward(self, X, lengths, **kwargs):
+        # [Batch, feat_vec_len, Max_length] -> [Max_length, Batch, feat_vec_len]
+        output = X.transpose(1, 2).transpose(0, 1)
+        batch_size = len(lengths)
+        for i in range(self.depth):
+            # pack_padded_sequence so that padded items in the sequence won't be shown to the LSTM
+            output = pack_padded_sequence(output, lengths)
+            # If we don't send hidden and cell state to LSTM, they default to zero
+            # [Max_length, Batch, hidden_sizes[i-1]] -> [Max_length, Batch, 2 * hidden_sizes[i]]
+            # If i is 0 then hidden_sizes[i-1] is self.feat_vec_len
+            output, _ = self.brnn[i](output)
+            # undo the packing operation
+            output, _ = pad_packed_sequence(output)
+            # We need to change our last output dimension. We'll use averaging
+            # [Max_length, Batch, 2 * hidden_sizes[i]] -> [Max_length, Batch, hidden_sizes[i]]
+            output = output.view(lengths[0], batch_size, 2, self.hidden_sizes[i])
+            output = output.mean(dim=2)
+            self.dropout(output)
 
         # [Max_length, Batch, hidden_sizes[-1]] -> [Batch, Max_length]
         output = output.transpose(0, 1).squeeze(dim=2)
